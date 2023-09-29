0864cf8dc0c8cea4ac28d99b946ee3a58fc21155
PrathamHusky07 <kulkarni.prath@northeastern.edu>
2023-09-29 19:47:45 -0400

    PyPDF implementation done

--- part_1/main.py
+++ part_1/main.py
@@ -1,5 +1,54 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+import requests
+from io import BytesIO
+from PyPDF2 import PdfReader
+import nougat
+import base64
 
-st.title('Uber pickups in NYC')
+# Function to extract text from a PDF using PyPDF2
+def extract_text_pypdf(pdf_file):
+    pdf_reader = PdfReader(pdf_file)
+    text = ""
+    for page in pdf_reader.pages:
+        text += page.extract_text()
+    return text
+
+# Function to summarize text using Nougat
+def summarize_text_nougat(text):
+    summarizer = nougat.Nougat()
+    summary = summarizer.summarize(text)
+    return summary
+
+st.title("PDF Summary App")
+
+# User input for the PDF link
+pdf_link = st.text_input("Enter the link to the PDF file:")
+
+# User selection for the PDF processing library
+pdf_library = st.radio("Select PDF processing library:", ("PyPDF2", "Nougat"))
+
+if st.button("Generate Summary"):
+    if pdf_link:
+        try:
+            # Download the PDF file
+            response = requests.get(pdf_link)
+            pdf_content = response.content
+
+            if pdf_library == "PyPDF2":
+                # Extract text using PyPDF2
+                text = extract_text_pypdf(BytesIO(pdf_content))
+                st.subheader("Summary:")
+                st.write(text)  # Display the extracted text
+            else:
+                # Extract text using Nougat
+                pdf_base64 = base64.b64encode(pdf_content).decode("utf-8")
+                text = pdf_base64
+                summary = summarize_text_nougat(text)
+                st.subheader("Summary:")
+                st.write(summary)
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+    else:
+        st.warning("Please enter a valid PDF link.")
