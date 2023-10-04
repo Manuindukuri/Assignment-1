76fbc4c627a2560b134c1ad80d9f691105bd0bb2
Manohar <indukuri.k@northeastern.edu>
2023-10-03 21:52:33 -0400

    Nougat code completion

--- part_1/main.py
+++ part_1/main.py
@@ -1,8 +1,8 @@
 import streamlit as st
+import subprocess
 import requests
 from io import BytesIO
 from PyPDF2 import PdfReader
-import nougat
 import base64
 
 # Function to extract text from a PDF using PyPDF2
@@ -13,27 +13,26 @@ def extract_text_pypdf(pdf_file):
         text += page.extract_text()
     return text
 
-# Function to summarize text using Nougat
-def summarize_text_nougat(text):
-    summarizer = nougat.Nougat()
-    summary = summarizer.summarize(text)
-    return summary
+def run_colab_notebook():
+    # Replace 'YOUR_COLAB_NOTEBOOK_URL' with the actual Colab notebook URL
+    colab_notebook_url = 'https://colab.research.google.com/drive/1m6HVtjNsZv_ru3n7ykfRDCmd73zhYAdA#scrollTo=JLXCdVCNULDm'
 
+<<<<<<< Updated upstream
 st.title("PDF Summary App")
 
 # User input for the PDF link
 pdf_link = st.text_input("Enter the link to the PDF file:")
+=======
+    # Prepare a shell command to open the Colab notebook with parameters
+    cmd = f"open -a 'Google Chrome' '{colab_notebook_url}'"
+>>>>>>> Stashed changes
 
-# User selection for the PDF processing library
-pdf_library = st.radio("Select PDF processing library:", ("PyPDF2", "Nougat"))
+    # Execute the shell command to open the notebook
+    subprocess.Popen(cmd, shell=True)
 
-if st.button("Generate Summary"):
-    if pdf_link:
-        try:
-            # Download the PDF file
-            response = requests.get(pdf_link)
-            pdf_content = response.content
+st.title("PDF Analysis")
 
+<<<<<<< Updated upstream
             if pdf_library == "PyPDF2":
                 # Extract text using PyPDF2
                 text = extract_text_pypdf(BytesIO(pdf_content))
@@ -52,3 +51,45 @@ if st.button("Generate Summary"):
 
     else:
         st.warning("Please enter a valid PDF link.")
+=======
+# Add a radio button to select the PDF processing library
+pdf_library = st.radio("Select PDF processing library:", ["PyPDF2", "Nougat"])
+
+# If "Nougat" is selected, hide the text box
+if pdf_library == "Nougat":
+    st.write("Nougat library selected. No need to enter a link here instead please press the button below")
+else:
+    # If "PyPDF2" or any other option is selected, show the text box
+    pdf_link = st.text_input("Enter the link to the PDF file:")
+
+if pdf_library == "Nougat":
+    if st.button("Run Colab Notebook"):
+        run_colab_notebook()
+        st.success("Colab notebook execution triggered successfully.")
+
+if pdf_library != "Nougat":
+    if st.button("Generate Summary"):
+        if pdf_link:
+            try:
+                # Download the PDF file
+                response = requests.get(pdf_link)
+                pdf_content = response.content
+
+                if pdf_library == "PyPDF2":
+                    # Extract text using PyPDF2
+                    text = extract_text_pypdf(BytesIO(pdf_content))
+                    st.subheader("Summary:")
+                    st.write(text)  # Display the extracted text
+                    
+                    # Calculate and display the length of the PDF and summary
+                    st.subheader("Length of PDF:")
+                    st.write(len(pdf_content))
+                    st.subheader("Length of Summary:")
+                    st.write(len(text))
+
+            except Exception as e:
+                st.error(f"An error occurred: {e}")
+
+        else:
+            st.warning("Please enter a valid PDF link.")
+>>>>>>> Stashed changes
